e006f9cedadcfefe0b3e326219b53d1878cae4e3
Duc Bui <39622487+5unof4Beach@users.noreply.github.com>
2022-02-26 01:02:52 +0700

    push b4 sleep

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-    document.getElementById("menu-the-loai").style.width = "250px";
+function moMenu() {
+    document.getElementById("menu-the-loai").style.width = "300px";
   }
   
   /* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
+function dongMenu() {
     document.getElementById("menu-the-loai").style.width = "0";
   }
