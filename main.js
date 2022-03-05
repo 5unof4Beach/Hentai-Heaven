8f7ed5780b75e8ec9b7ec71b2b058e63d33dd17e
5unof4Beach <suckmydick2001gta@gmail.com>
2022-03-05 15:09:16 +0700

    changes

--- main.js
+++ main.js
@@ -10,4 +10,14 @@ function dongMenu() {
     // a.style.height = "0px";
 }
 
-console.log(window)
+function themTruyen() {
+  var box = document.createElement('div')
+  box.className = 'truyen'
+  box.style.height = '340px';
+  box.style.width = '300px';
+  box.style.border = '1px solid black';
+  
+  var ds_truyen = document.getElementById('danh-muc-truyen');
+  
+  ds_truyen.appendChild(box);
+}
