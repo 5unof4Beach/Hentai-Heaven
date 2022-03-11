594ffb3fef57691fb91703bd8c8964f6d18e35ed
5unof4Beach <suckmydick2001gta@gmail.com>
2022-03-11 10:02:14 +0700

    update

--- learning JS.js
+++ learning JS.js
@@ -4,6 +4,10 @@ chiaHetchoBa = (num) =>{
 
 console.log(chiaHetchoBa(5));
 
-var students = ['123', '243124'];
+var students = ['123', '243124', 'duc'];
+
+students.unshift('111111');
+var first = students.shift();
 
 console.log(students[1]);
+console.log(first);
